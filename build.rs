aca2eecefc2281d8c2d47336941b485fa7df6d35
BeanFlame (豆焰) <beanflame@qq.com>
2023-08-03 10:44:13 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ fn main() {
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/neovide.ico");
-        res.compile().expect("Could not attach exe icon Card2");
+        res.compile().expect("Could not attach exe icon");
     }
 }
